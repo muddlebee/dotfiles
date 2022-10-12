dcd6f5e2b04eff39896f5d7019c59a74be17ead7
Anwesh <anweshknayak@gmail.com>
2022-10-13 00:44:34 +0530

    Create resource-monitor.sh

---
+++ ubuntu-20/resource-monitor.sh
@@ -0,0 +1,3 @@
+#https://askubuntu.com/questions/293426/what-system-monitoring-tools-are-available
+
+curl -L https://raw.githubusercontent.com/nicolargo/glancesautoinstall/master/install.sh | sudo /bin/bash
