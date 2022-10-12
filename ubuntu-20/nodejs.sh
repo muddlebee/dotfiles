e39245a6c9d1bd07383d2527e0f937676b08a092
Anwesh <anweshknayak@gmail.com>
2022-10-13 00:43:29 +0530

    Create nodejs.sh

---
+++ ubuntu-20/nodejs.sh
@@ -0,0 +1,5 @@
+#https://linuxize.com/post/how-to-install-node-js-on-ubuntu-20-04/
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm list-remote
+nvm install node
