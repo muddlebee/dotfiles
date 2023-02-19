4956c6d5a5eb10cab3ba1bc6c23e8c773338e298
Anwesh <anweshknayak@gmail.com>
2023-02-19 19:46:35 +0530

    Update nodejs.sh

--- ubuntu-20/nodejs.sh
+++ ubuntu-20/nodejs.sh
@@ -1,5 +1,7 @@
 #https://linuxize.com/post/how-to-install-node-js-on-ubuntu-20-04/
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source /root/.bashrc
 nvm list-remote
-nvm install node
+nvm install node v14.21.3
+
