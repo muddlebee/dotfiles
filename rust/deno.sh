394dcd75215c667d9e24cb1fa6631b891b620bf0
Anwesh <anweshknayak@gmail.com>
2024-01-09 15:05:14 +0530

    Update deno.sh

--- rust/deno.sh
+++ rust/deno.sh
@@ -3,7 +3,6 @@
 # Install Rust using rustup
 echo "Installing Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-sudo apt install cargo
 # Ensure Cargo bin directory is in the PATH
 source $HOME/.cargo/env
 
@@ -11,13 +10,15 @@ source $HOME/.cargo/env
 echo "Downloading and installing LLVM version 16..."
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
-sudo ./llvm.sh 16
+echo | sudo ./llvm.sh 16
 
 # Clean up
 rm llvm.sh
 
 sudo apt install --install-recommends -y cmake libglib2.0-dev
 sudo apt install -y protobuf-compiler
+sudo apt install cargo
+
 protoc --version  # Ensure compiler version is 3+
 
 git config --global core.symlinks true
